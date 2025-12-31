c120a38b448ca3455ee18c9a1491c16571a7ec79
jpwirzt10@gmail.com <jpwirzt10@gmail.com>
2025-12-31 10:58:17 -0300

    cambios

--- native-qr-scanner/Package.swift
+++ native-qr-scanner/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "WbsistemasNativeQrScanner",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "WbsistemasNativeQrScanner",
